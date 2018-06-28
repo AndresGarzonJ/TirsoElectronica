15484d3341412895af13b2b20981dbc3b7b8bb96
AndresGJ144 <andres_sgj144@unicauca.edu.co>
2018-06-28 13:00:36 -0500

    Notas mejoras
    Notas version.txt

--- app/Providers/RepositoryServiceProvider.php
+++ app/Providers/RepositoryServiceProvider.php
@@ -34,7 +34,7 @@ use App\Shop\Provinces\Repositories\Interfaces\ProvinceRepositoryInterface;
 use App\Shop\Provinces\Repositories\ProvinceRepository;
 use Illuminate\Support\ServiceProvider;
 use App\Shop\Blogs\Repositories\Interfaces\BlogRepositoryInterface;
-use App\Shop\Blogs\Repositories\BlogRepository;
+use App\Shop\Blogs\Repositories\BlogRepository; 
 
 class RepositoryServiceProvider extends ServiceProvider
 {
